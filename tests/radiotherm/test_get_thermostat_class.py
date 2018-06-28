556d0dba41ad8971d7f1967ec3c21dab24a62825
Clayton Craft <clayton@craftyguy.net>
2018-06-27 19:06:18 -0700

    tests: Implement support for allowing more generic thermostats
    This tests cases where a thermostat matches revision or model family, but is not
    an exact match of Model Rev Version

--- tests/radiotherm/test_get_thermostat_class.py
+++ tests/radiotherm/test_get_thermostat_class.py
@@ -1,11 +1,20 @@
 from tests.base_test_case import BaseTestCase
 import radiotherm
-from radiotherm.thermostat import CT50v194
+from radiotherm.thermostat import CT50v194, CT80RevB, CT80
+
 
 class TestGetThermostatClass(BaseTestCase):
     def test_class_exists(self):
         ret = radiotherm.get_thermostat_class('CT50 V1.94')
         self.assertEqual(ret, CT50v194)
+
+    def test_class_base_rev_exists(self):
+        ret = radiotherm.get_thermostat_class('CT80 RevB V5.94')
+        self.assertEqual(ret, CT80RevB)
+
+    def test_class_base_model_exists(self):
+        ret = radiotherm.get_thermostat_class('CT80 RevA V2.94')
+        self.assertEqual(ret, CT80)
 
     def test_class_does_not_exist(self):
         ret = radiotherm.get_thermostat_class('CT51 V3.17')
